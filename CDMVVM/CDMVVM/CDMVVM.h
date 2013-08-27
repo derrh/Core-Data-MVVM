9b7a699698174b490dda3e156b84f7cc20166902
Derrick J. Hathaway <derrick@instructure.com>
2013-08-27 08:26:09 -0600

    Added a single inclusion header.

---
+++ CDMVVM/CDMVVM/CDMVVM.h
@@ -0,0 +1,14 @@
+//
+//  CDMVVM.h
+//  CDMVVM
+//
+//  Created by derrick on 8/23/13.
+//  Copyright (c) 2013 Derrick Hathaway. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+#import "DHCollectionViewModel.h"
+#import "DHTableViewAdapter.h"
+#import "DHTableViewController.h"
+#import "DHCollectionViewAdapter.h"
+#import "DHCollectionViewController.h"
